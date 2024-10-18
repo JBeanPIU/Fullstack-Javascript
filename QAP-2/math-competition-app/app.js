fe46ac5ff6220318c58726e6a71c97315009d082
Cameron Beanland <cameronbeanland@hotmail.com>
2024-10-18 19:20:16 -0230

    Update app.js
    setup the basics for this one, i think i got everything done up lol 
    
    -setup ejs paths
    -mostly it really

--- QAP-2/math-competition-app/app.js
+++ QAP-2/math-competition-app/app.js
@@ -2,3 +2,27 @@
 // By: Cameron Beanland 
 // Date: October 18th. 2024 
 
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+/* -- EJS setup -- */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true })); // used for form submissions
+app.use(express.static(path.join(__dirname, 'public'))); // gives static files
+
+
+/* -- Routes setup -- */
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter); // links outside of folder, finds routes and catches index.js
+
+app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`);
+})
+
+// mostly used to setup all the separate pages*
