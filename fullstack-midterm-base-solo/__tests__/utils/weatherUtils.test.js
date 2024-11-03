d3936327e4084b3387448cb066103b952e0fce42
Cameron Beanland <cameronbeanland@hotmail.com>
2024-11-03 01:02:55 -0230

    Update weatherUtils.test.js
    added a few tests for the generateRandomMenuItem function, and spaced things out to be clearer (or at least i think)
    
    // TO DO: still complete code to test other two functions lol

--- fullstack-midterm-base-solo/__tests__/utils/weatherUtils.test.js
+++ fullstack-midterm-base-solo/__tests__/utils/weatherUtils.test.js
@@ -2,25 +2,47 @@
 // By: Cameron Beanland
 // Date: November 3rd, 2024
 
+
 const {Cuisines, Dishes} = require("../../utils/data");
 const {generateRandomMenuItem, generateMenu, selectRandomCuisine} = require("../../utils/restaurantUtils");
 
-describe('Restaurant Functions', () => {
-    describe('generateRandomMenuItem', () => {
-      test('if this was done right, it should generate a random menu item with Italian food', () => {
-        const cuisine = 'Italian'; // chose this because i love italian food
-        const menuItem = generateRandomMenuItem(cuisine);
 
-        // checks to verify that menu item has required attributes
-        expect(menuItem).toHaveProperty('name');
-        expect(menuItem).toHaveProperty('description');
-      })
+/**--------------------------------------------------------------------------------------------------------------**/
+//* RANDOM MENU ITEM TESTINGGGGGGG
+describe('Restaurant Functions', () => {
+  describe('generateRandomMenuItem', () => { // 1st test
+    test('should generate a random menu item with Italian food', () => {
+      const cuisine = 'Italian'; // chose this because I love Italian food
+      const menuItem = generateRandomMenuItem(cuisine);
+
+      // checks to verify that menu item has required attributes
+      expect(menuItem).toHaveProperty('name');
+      expect(menuItem).toHaveProperty('description');
     });
 
+    test('should select a random cuisine type from data.js', () => { // 2nd test
+      const cuisine = selectRandomCuisine(); 
+      
+      // verifies that selected cuisine comes from data.js
+      expect(Object.keys(Dishes)).toContain(cuisine);
+    });
+
+    test('should throw an error for invalid cuisine', () => { // 3rd test
+      expect(() => generateRandomMenuItem('FakeCuisine')).toThrow('Cuisine lost in the sauce');
+    });
+  });
+
+
+
+/**--------------------------------------------------------------------------------------------------------------**/
+//* GENERATE MENU TESTINGGGGGGGGGGGGG
     describe('generateMenu', () => {
       // Test implementations go here
     });
 
+
+/**--------------------------------------------------------------------------------------------------------------**/
+//* RANDOM CUISINE SELECTION TESTINGGGGGGGGGGGGGGGGGGGGGGGGGG
     describe('selectRandomCuisine', () => {
       // Test implementations go here
     });
